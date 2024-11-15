c77ef5d17cdbe2d97298f4229b804b466481b586
Anshu Pitlia <anshupitlia@gmail.com>
2024-11-15 17:51:42 +0530

    Add put method for updating a product. This method will go through further changes.

--- src/main/java/com/ikea/assignment/product_information_system/controller/ProductsController.java
+++ src/main/java/com/ikea/assignment/product_information_system/controller/ProductsController.java
@@ -1,13 +1,14 @@
 package com.ikea.assignment.product_information_system.controller;
 
+import com.ikea.assignment.product_information_system.model.Country;
 import com.ikea.assignment.product_information_system.model.Product;
 import com.ikea.assignment.product_information_system.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,13 +17,40 @@ public class ProductsController {
     @Autowired
     private ProductRepository productRepository;
 
+    private Product faltuSetup() {
+        Product product = new Product();
+        product.setSku("1");
+        product.setTitle("Title");
+        product.setPrice(BigDecimal.TEN);
+        product.setCountry(Country.EU);
+        product.setDiscountPercentage(2.0);
+        product.setImageUrl("https://assets.dummyjson.com/public/qr-code.png");
+        return product;
+    }
     @GetMapping
     public List<Product> getProducts() {
-        return productRepository.findAll();
+        Product product = faltuSetup();
+        List<Product> products = new ArrayList<>();
+        products.add(product);
+        return products;
+        //return productRepository.findAll();
     }
 
     @GetMapping(value="/{sku}")
     public Product getProduct(@PathVariable String sku) {
-        return productRepository.findById(sku).orElseThrow(RuntimeException::new);
+        return faltuSetup();
+        //return productRepository.findById(sku).orElseThrow(RuntimeException::new);
+    }
+
+    @PutMapping("/{sku}")
+    public ResponseEntity updateProduct(@PathVariable String sku, @RequestBody Product product) {
+        Product currentProduct = productRepository.findById(sku).orElseThrow(RuntimeException::new);
+        currentProduct.setCountry(product.getCountry());
+        currentProduct.setPrice(product.getPrice());
+        currentProduct.setDiscountPercentage(product.getDiscountPercentage());
+        System.out.println("This product will be saved" + currentProduct.toString());
+        Product newProduct = productRepository.save(currentProduct);
+
+        return ResponseEntity.ok(newProduct);
     }
 }
